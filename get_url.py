1a1fcf5ceb7cc48f57b9d168ab86b73403d09f90
tiktaak <aramina1414@gmail.com>
2023-11-11 18:04:31 +0330

    fix "cannot find the file specified" error for get_url.py

--- get_url.py
+++ get_url.py
@@ -2,6 +2,6 @@ import os
 import subprocess
 
 # os.system('jupyter server list')
-raw_string = subprocess.check_output('cd env && cd Scripts && activate && cd ../.. && jupyter server list').decode('utf-8').split(" :: ")
+raw_string = subprocess.check_output('jupyter server list').decode('utf-8').split(" :: ")
 
 print((raw_string[0][28:]))
